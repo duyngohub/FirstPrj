327492c3175c423045e30639ce48b9340bee5385
NGO KHUONG DUY <khuongduyngo381@gmail.com>
2024-08-16 13:06:17 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Ngo Khuong Duy!");
+        System.out.println("Welcome to the DHHTTT19A!");
+    }
+}
